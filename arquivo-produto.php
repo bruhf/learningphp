3ce0f8f3f2135021a499f5141b4b8f9876694f50
Bruna Fidelis <bruhf_nasc@hotmail.com>
2019-04-17 20:56:18 -0300

    commit2

--- arquivo-produto.php
+++ arquivo-produto.php
@@ -1,8 +1,11 @@
 <html> 
     <?php $produto = "Produto - Produto adicionado com sucesso"; ?>
     Produto <?php echo $produto; ?>
+    <?php $preco = 
     <!-- <?php
     $nome = $_GET["nome"];
+    $preco = $_GET["preco"];
+    Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
     ?>
-    Produto <?php echo $nome; ?> adicionado com sucesso! -->
+    Produto <?php echo $nome; ?> adicionado com sucesso! //posso usar assim tbm, esse code faz a mesma coisa que o de cima -->
  </html>   
