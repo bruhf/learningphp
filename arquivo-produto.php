610accd13ea3327268ef7e899b2214c4ea0ee0ce
Bruna Fidelis <bruhf_nasc@hotmail.com>
2019-04-17 20:55:12 -0300

    second

--- arquivo-produto.php
+++ arquivo-produto.php
@@ -1,4 +1,8 @@
 <html> 
     <?php $produto = "Produto - Produto adicionado com sucesso"; ?>
     Produto <?php echo $produto; ?>
+    <!-- <?php
+    $nome = $_GET["nome"];
+    ?>
+    Produto <?php echo $nome; ?> adicionado com sucesso! -->
  </html>   
